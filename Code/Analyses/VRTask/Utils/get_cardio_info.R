786a5f171fa696df804753984c5adc14d4383d1c
Pawel Motyka <pawel.motyka@psych.uw.edu.pl>
2020-08-13 17:28:41 +0200

    integration

--- Code/Analyses/VRTask/Utils/get_cardio_info.R
+++ Code/Analyses/VRTask/Utils/get_cardio_info.R
@@ -180,13 +180,13 @@ get_cardio_info <- function(fulldat, subj_ID) {
     ECG_dat$tend[i] <- twave2[tend,1] # T wave end position
     ECG_dat$systolength[i] <- twave2[tend,1]- ECG_dat$RPm1[i] # Length of systole 
     
-    #Optional: Plot visualizations of T-wave end for each trial and export them as jpg files
-    jpeg(file = paste0(here(),'/Data/VRTask/Cardio/ExpSubjects/T_waves_plots/VRCC_t_wave_',subj_ID,'_trial', i,".jpg"), width=1024, height=600)
-    plot(twave_long,col='black',xlab='Time (ms)', ylab= 'Electric potential (ECG)', cex.lab = 1.5, cex.axis = 1.4)
-    points(twave_int[tmaxpos,1],twave_int[tmaxpos,2],col='purple3',pch='+',cex=6)
-    points(twave2[tend,1],twave2[tend,2],col='orangered',pch='+',cex=6)
-    title(paste('ID',subj_ID,'_trial ', i, sep=''),cex.main = 2, line=-2, outer=TRUE)
-    dev.off()
+    # Optional: Plot visualizations of T-wave end for each trial and export them as jpg files
+    # jpeg(file = paste0(here(),'/Data/VRTask/Cardio/ExpSubjects/T_waves_plots/VRCC_t_wave_',subj_ID,'_trial', i,".jpg"), width=1024, height=600)
+    # plot(twave_long,col='black',xlab='Time (ms)', ylab= 'Electric potential (ECG)', cex.lab = 1.5, cex.axis = 1.4)
+    # points(twave_int[tmaxpos,1],twave_int[tmaxpos,2],col='purple3',pch='+',cex=6)
+    # points(twave2[tend,1],twave2[tend,2],col='orangered',pch='+',cex=6)
+    # title(paste('ID',subj_ID,'_trial ', i, sep=''),cex.main = 2, line=-2, outer=TRUE)
+    # dev.off()
     
     } else {
       
