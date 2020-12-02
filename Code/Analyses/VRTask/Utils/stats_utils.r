942915566ebefc9f0beaa8c14e1910fbf3829740
Aleksander Molak <aleksander.molak@lingarogroup.com>
2020-12-02 22:58:37 +0100

    Added stats_utils.r

---
+++ Code/Analyses/VRTask/Utils/stats_utils.r
@@ -0,0 +1,19 @@
+
+# Author         : Aleksander Molak
+# Date           : Dec 1, 2020
+
+
+library(lme4)
+library(performance)
+
+get_ICC = function(model) {
+  # Computes intraclass correlation
+  var_ = as.numeric(VarCorr(model)) 
+  cat(var_[1] / (var_[1] + var_[2]))
+}
+
+get_r2_mlm <- function(model) {
+  res <- r2_nakagawa(model)
+  cat("Variance explained by the model (conditional Nakagawa's R2): ", res$R2_conditional, "\n")
+  cat("Variance explained by fixed effects (marginal Nakagawa's R2): ", res$R2_marginal, "\n")
+}
