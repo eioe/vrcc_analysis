0e08714ccfb4846981d2858d6900b1693a2c40ee
Aleksander Molak <aleksander.molak@lingarogroup.com>
2021-04-22 21:01:13 +0200

    Adding circular analyses

--- Code/Analyses/VRTask/Utils/stats_utils.r
+++ Code/Analyses/VRTask/Utils/stats_utils.r
@@ -5,6 +5,7 @@
 
 library(lme4)
 library(performance)
+library()
 
 # get_ICC = function(model) {
 #   # Computes intraclass correlation
@@ -16,4 +17,11 @@ get_r2_mlm <- function(model) {
   res <- r2_nakagawa(model)
   cat("Variance explained by the model (conditional Nakagawa's R2): ", res$R2_conditional, "\n")
   cat("Variance explained by fixed effects (marginal Nakagawa's R2): ", res$R2_marginal, "\n")
+}
+
+get_circular_components <- function(x, period) {
+  omega <- (2 * pi) / period
+  sin_component = sin(x * omega)
+  cos_component = cos(x * omega)
+  data.frame(sin_component, cos_component)
 }
